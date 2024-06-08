318fdbdcaede19890bc3bfa2db22eb0fee458a5b
keengsleyudeh <keengsleyudeh@gmail.com>
2024-06-08 11:10:58 -0700

    The end

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import OrderItem from "./OrderItem"
 import {
@@ -8,11 +8,20 @@ import {
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
+
+  const fetcher = useFetcher()
+
+  useEffect(function() {
+    if(!fetcher.data && fetcher.state === 'idle')
+    fetcher.load('/menu')
+  },[fetcher])
   
   const {
     id,
@@ -48,7 +57,13 @@ function Order() {
         <p className="text-xs text-stone-500">(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
       <ul className="dive-stone-200 divide-y border-b border-t">
-        {cart.map((item) => (<OrderItem item={item} key={item.pizzaId} />))}
+        {cart.map((item) =>
+          (<OrderItem 
+          item={item} 
+          key={item.pizzaId} 
+          ingredients={fetcher.data?.find((el)=>el.id === item.pizzaId).ingredients ?? []}
+          isLoadingIngredients = {fetcher.state === 'loading'}
+           />))}
       </ul>
 
       <div className="space-y-2 bg-stone-200 px-6 py-5">
@@ -56,6 +71,7 @@ function Order() {
         {priority && <p className="text-sm font-medium text-stone-600">Price priority: {formatCurrency(priorityPrice)}</p>}
         <p className="font-bold">To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
+      {!priority && <UpdateOrder order={order}/>}
     </div>
   );
 }
